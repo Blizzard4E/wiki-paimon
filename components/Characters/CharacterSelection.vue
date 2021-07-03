545b295fa1ee793ed9550b79ad172e18c6713a06
Blizzard4E <phenggamingone@gmail.com>
2021-07-04 01:03:43 +0700

    added Kazuha and Freedom sworn sword

--- components/Characters/CharacterSelection.vue
+++ components/Characters/CharacterSelection.vue
@@ -190,7 +190,7 @@ export default {
     },
     mounted() {
         this.characters = characters;
-        this.updateSelectedChar('aether');
+        this.updateSelectedChar('kazuha');
     }
 }
 </script>
